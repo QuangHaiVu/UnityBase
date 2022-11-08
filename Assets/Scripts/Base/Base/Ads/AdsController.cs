4e5647e8e03201701e34009cdc08e125f01bd32e
QuangHaiVu <zzquanghai0108zz@gmail.com>
2022-11-08 11:30:06 +0700

    new Ads Controller

--- Assets/Scripts/Base/Base/Ads/AdsController.cs
+++ Assets/Scripts/Base/Base/Ads/AdsController.cs
@@ -1,127 +1,210 @@
+﻿using UnityEngine;
 using System;
-using UnityEngine;
+using EasyMobile;
 
 public class AdsController : PersistentSingleton<AdsController>
 {
-    [SerializeField] private AdsRewardVideoHelper reward;
-    [SerializeField] private AdsInterstitialHelper interstitial;
-    [SerializeField] private AdsBannerHelper banner;
-
-    [SerializeField] private bool isUsingAds;
-
-    private void Start()
+    [SerializeField] private BannerAdPosition bannerPosition;
+    [SerializeField] private float timeToShowAds = 10f;
+    public float TimeToShowAds
     {
-        if(!isUsingAds) return;
-        InitService();
+        get => timeToShowAds;
+        set => timeToShowAds = value;
     }
 
-    private void InitService()
+    private static DateTime lastTimeShowAd = DateTime.Now;
+
+    public static Action OnInterShow = null;
+    public static Action OnRewardShow = null;
+
+    private Action onInterstitialComplete = null;
+    private Action onInterstitialDefaultAction = null;
+    private Action onRewardComplete = null;
+    private Action onRewardSkip = null;
+
+    private void OnEnable()
     {
-        Debug.Log("MyAppStart Start called");
+        Advertising.InterstitialAdCompleted += OnInterstitialAdCompleted;
+        Advertising.RewardedAdCompleted += OnRewardedAdCompleted;
+        Advertising.RewardedAdSkipped += OnRewardedAdSkipped;
+    }
 
-        //Dynamic config example
-        IronSourceConfig.Instance.setClientSideCallbacks(true);
+    void OnDisable()
+    {
+        Advertising.InterstitialAdCompleted -= OnInterstitialAdCompleted;
+        Advertising.RewardedAdCompleted -= OnRewardedAdCompleted;
+        Advertising.RewardedAdSkipped -= OnRewardedAdSkipped;
+    }
 
-        string id = IronSource.Agent.getAdvertiserId();
-        Debug.Log("IronSource.Agent.getAdvertiserId : " + id);
-
-        Debug.Log("IronSource.Agent.validateIntegration");
-        IronSource.Agent.validateIntegration();
-
-        Debug.Log("unity version" + IronSource.unityVersion());
-
-
-        var developerSettings =
-            Resources.Load<IronSourceMediationSettings>(IronSourceConstants.IRONSOURCE_MEDIATION_SETTING_NAME);
-        if (developerSettings != null)
+    public void OnApplicationPause(bool paused)
+    {
+        if (paused)
         {
-#if UNITY_ANDROID
-            string appKey = developerSettings.AndroidAppKey;
-#elif UNITY_IOS
-			string appKey = developerSettings.IOSAppKey;
-#endif
-            Debug.Log("App key :" + appKey);
-
-            if (appKey.Equals(string.Empty))
-            {
-                Debug.LogWarning("IronSourceInitilizer Cannot init without AppKey");
-            }
-            else
-            {
-                IronSource.Agent.init(appKey);
-                InitHelper();
-            }
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
         }
     }
 
-    private void InitHelper()
+    private void OnApplicationFocus(bool hasFocus)
     {
-        reward.init();
-        interstitial.Init();
-        banner.Init();
+        if (hasFocus)
+        {
+            Time.timeScale = 1;
+        }
+        else
+        {
+            Time.timeScale = 0;
+        }
     }
 
-    void OnApplicationPause(bool isPaused)
-    {
-        IronSource.Agent.onApplicationPause(isPaused);
-    }
-
-    #region PUBLIC METHOD
-
-    public void ShowReward(Action onComplete)
-    {
-        if (!isUsingAds)
-        {
-            onComplete?.Invoke();
-            return;
-        }
-        if (!reward)
-        {
-            Debug.LogError("Reward not assigned");
-            return;
-        }
-
-        reward.ShowRewardedVideo(onComplete);
-    }
-
-    public void ShowInterstitial(Action onShow)
-    {
-        if (!isUsingAds)
-        {
-            onShow?.Invoke();
-            return;
-        }
-        if (!interstitial)
-        {
-            Debug.LogError("Interstitial not assigned");
-            return;
-        }
-
-        interstitial.ShowInterstitial(onShow);
-    }
+    #region Banner
 
     public void ShowBanner()
     {
-        if(!isUsingAds) return;
-        if (!banner)
-        {
-            Debug.LogError("Banner not assigned");
-            return;
-        }
-
-        banner.ShowBanner();
+        Advertising.ShowBannerAd(bannerPosition, BannerAdSize.Banner);
     }
 
     public void HideBanner()
     {
-        if(!isUsingAds) return;
-        if (!banner)
-        {
-            Debug.LogError("Banner not assigned");
-            return;
-        }
+        Advertising.HideBannerAd();
+    }
 
-        banner.HideBanner();
+    public void DestroyBanner()
+    {
+        Advertising.DestroyBannerAd();
+    }
+
+    #endregion
+
+    #region VideoReward
+
+    public bool IsRewardedAdReady()
+    {
+        Debug.Log($"unity-script: AdsRewardedVideoAvailable - {Advertising.IsRewardedAdReady()}");
+        return Advertising.IsRewardedAdReady();
+    }
+
+    public void ShowRewardedVideo(Action onComplete, Action onSkip = null)
+    {
+        onRewardComplete = onComplete;
+        onRewardSkip = onSkip;
+
+#if UNITY_EDITOR
+        ShowRewardOnUnityEditor();
+#else
+        ShowRewardOnMobile();
+#endif
+    }
+
+    private void ShowRewardOnUnityEditor()
+    {
+        onRewardComplete?.Invoke();
+        onRewardComplete = null;
+    }
+
+    private void ShowRewardOnMobile()
+    {
+        if (IsRewardedAdReady())
+        {
+            OnRewardShow?.Invoke();
+            Advertising.ShowRewardedAd();
+        }
+        else
+        {
+            Advertising.LoadRewardedAd();
+        }
+    }
+
+    private void OnRewardedAdCompleted(RewardedAdNetwork arg1, AdPlacement arg2)
+    {
+        onRewardComplete?.Invoke();
+        onRewardComplete = null;
+
+        onRewardSkip = null;
+    }
+
+    private void OnRewardedAdSkipped(RewardedAdNetwork arg1, AdPlacement arg2)
+    {
+        onRewardComplete = null;
+
+        onRewardSkip?.Invoke();
+        onRewardSkip = null;
+    }
+
+    #endregion
+
+    #region Interstitial
+
+    public bool IsInterstitialAdReady()
+    {
+        Debug.Log($"unity-script: AdsInterstitialReady - {Advertising.IsInterstitialAdReady()}");
+        return Advertising.IsInterstitialAdReady();
+    }
+
+    public void ShowInterstitial(Action onDefault = null, Action onComplete = null)
+    {
+        onInterstitialDefaultAction = onDefault;
+        onInterstitialComplete = onComplete;
+
+#if UNITY_EDITOR
+        ShowInterstitialOnUnityEditor();
+#else
+        ShowInterstitialOnMobile();
+#endif
+    }
+
+    private void ShowInterstitialOnUnityEditor()
+    {
+        onInterstitialDefaultAction?.Invoke();
+        onInterstitialDefaultAction = null;
+        
+        onInterstitialComplete?.Invoke();
+        onInterstitialComplete = null;
+    }
+
+    private void ShowInterstitialOnMobile()
+    {
+        if (IsInterstitialAdReady() && CanShowAds())
+        {
+            OnInterShow?.Invoke();
+            Advertising.ShowInterstitialAd();
+            CalculateTimeAdsShowSuccess();
+        }
+        else
+        {
+            Advertising.LoadInterstitialAd();
+
+            onInterstitialDefaultAction?.Invoke();
+            onInterstitialDefaultAction = null;
+        }
+    }
+
+    private void OnInterstitialAdCompleted(InterstitialAdNetwork arg1, AdPlacement arg2)
+    {
+        onInterstitialDefaultAction?.Invoke();
+        onInterstitialDefaultAction = null;
+
+        onInterstitialComplete?.Invoke();
+        onInterstitialComplete = null;
+    }
+
+    #endregion
+
+    #region Helper
+
+    private void CalculateTimeAdsShowSuccess()
+    {
+        lastTimeShowAd = DateTime.Now;
+    }
+    
+    private bool CanShowAds()
+    {
+        var timeLastAds = (float)(DateTime.Now - lastTimeShowAd).TotalSeconds;
+        Debug.Log($"Time since last ads: {timeLastAds}. Can show ads: {timeLastAds >= timeToShowAds}");
+        return timeLastAds >= timeToShowAds;
     }
 
     #endregion
